bf9b98f4990cd1208632ef2cb38a282dc6ccecae
Mrudula <kondrajumrudula1@gmail.com>
2026-03-17 22:55:55 +0530

    Create numbers.py

---
+++ numbers.py
@@ -0,0 +1,11 @@
+result = "{}{}".format(145, 'o')
+print(result)   # Output: 145o
+radius = 84
+area = 3.14 * radius * radius
+print("Area:", int(area))
+water = area * 1.4
+print("Water:", int(water))
+distance = 490
+time = 7 * 60   # seconds
+speed = distance / time
+print("Speed:", int(speed))
