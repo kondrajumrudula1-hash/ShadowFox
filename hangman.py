06e687107024260ffa0540ff885f9704f84d4b4b
Mrudula <kondrajumrudula1@gmail.com>
2026-03-29 12:29:05 +0530

    Create hangman.py

---
+++ hangman.py
@@ -0,0 +1,38 @@
+import random
+
+words = ["python", "internship", "hangman", "developer", "shadowfox"]
+word = random.choice(words)
+
+guessed = []
+attempts = 6
+
+print("Welcome to Hangman!")
+
+while attempts > 0:
+    display = ""
+    for letter in word:
+        if letter in guessed:
+            display += letter + " "
+        else:
+            display += "_ "
+    
+    print("\nWord:", display)
+
+    if "_" not in display:
+        print("🎉 You won!")
+        break
+
+    guess = input("Guess a letter: ").lower()
+
+    if guess in guessed:
+        print("Already guessed!")
+        continue
+
+    guessed.append(guess)
+
+    if guess not in word:
+        attempts -= 1
+        print("Wrong guess! Attempts left:", attempts)
+
+if attempts == 0:
+    print("❌ You lost! Word was:", word)
